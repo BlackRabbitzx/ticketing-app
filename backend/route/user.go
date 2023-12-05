d44d2b0398c9c1d422d281e954c00f6c6a083484
Jennifer M <80823665+BlackRabbitzx@users.noreply.github.com>
2023-12-05 18:28:59 +0700

    Create user.go

---
+++ backend/route/user.go
@@ -0,0 +1,10 @@
+package route
+
+import (
+	"github.com/BlackRabbitzx/ticketing-app/backend/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoute(router *gin.Engine) {
+	router.POST("/signup", controller.InsertUser)
+}
