8b263d6afca6ed296e6d6e2a875e4f208dbe6135
Timjapri <timothy.japri@binus.ac.id>
2023-12-07 17:04:23 +0700

    payment page

---
+++ frontend/app/New changes/Payment.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './payment.css';
+
+import cover from './assets/download.jpg';
+
+const Payment = () => {
+
+
+  return (
+    <div className='payment-container'>
+      
+    </div>
+  );
+};
+
+export default Payment;
